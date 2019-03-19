f4b8c6455200cefe2acba8e5341b3866abfb0886
Sarp ISIK <sarp_isik91@hotmail.com>
2019-03-19 20:27:34 +0100

    Add video search

--- src/index.js
+++ src/index.js
@@ -21,89 +21,86 @@ if ('serviceWorker' in navigator) {
 
 const getElementByClass = className => document.querySelector(`.${className}`)
 const getElementById = id => document.getElementById(id)
-
-const doSearch = e => {
-  e.preventDefault()
-  console.log(searchInput.value)
-}
-
-const searchInput = getElementById('search')
-
-getElementById('search-form').addEventListener('submit', doSearch)
-
 const createNode = (element, ...rest) => {
   let el = document.createElement(element)
   el.classList.add(...rest)
 
   return el
 }
-
 const append = (parrent, element) => parrent.appendChild(element)
 
-const logError = error => console.log(error)
-
-const END_POINT = 'https://api.nasa.gov/planetary/apod?api_key='
-const API_KEY = 'twWX80n1gwefAwda1ekpVnxeX5jEGasGfYhCflGY'
-
-// fetch(END_POINT + API_KEY)
-//   .then(resp => resp.json())
-//   .then(showData)
-//   .catch(logError)
-
-const NASA_URL = 'https://images-api.nasa.gov/search?q=earth&media_type=image'
-fetch()
-  .then(resp => resp.json())
-  .then(data => {
-    const items = data.collection.items
-    items.forEach(item => {
-      let img = createNode('img'),
-        h1 = createNode('h1'),
-        small = createNode('small'),
-        p = createNode('p'),
-        header = createNode('div', 'header'),
-        image = createNode('div', 'image'),
-        text = createNode('div', 'text'),
-        wrapper = createNode('div', 'wrapper')
-
-      img.src = item.links[0].href
-      img.alt = item.data[0].title
-      p.innerHTML = item.data[0].description
-      h1.innerHTML = item.data[0].title
-      small.innerHTML = '<br />' + item.data[0].date_created
-
-      const intro = getElementByClass('intro')
-
-      append(h1, small)
-      append(header, h1)
-      append(image, img)
-      append(text, p)
-      append(wrapper, header)
-      append(wrapper, image)
-      append(wrapper, text)
-      append(intro, wrapper)
-    })
-  })
-  .catch(error => console.log(error))
-
-const showData = data => {
-  console.log(data)
-  let img = createNode('img'),
-    h1 = createNode('h1'),
-    h3 = createNode('h3'),
-    span = createNode('span'),
-    div = createNode('div')
-
-  img.src = data.url
-  img.alt = data.title
-  span.innerHTML = data.explanation
-  h1.innerHTML = data.title
-  h3.innerHTML = `${data.copyright} - ${data.date}`
-
-  const container = getElementByClass('intro')
-
-  append(div, h1)
-  append(div, h3)
-  append(div, img)
-  append(div, span)
-  append(container, div)
+const getMediaType = item => {
+  fetch(`https://images-api.nasa.gov/asset/${item.data[0].nasa_id}`)
+    .then(resp => resp.json())
+    .then(resp => console.log(resp))
+    .catch(err => console.log(err))
 }
+const doSearch = e => {
+  e.preventDefault()
+  document.getElementsByName('media').forEach(radio => {
+    radio.checked &&
+      fetch(
+        `https://images-api.nasa.gov/search?q=${searchInput.value}&media_type=${
+          radio.value
+        }`
+      )
+        .then(resp => resp.json())
+        .then(mediaLayout)
+        .catch(error => console.log(error))
+  })
+}
+
+const intro = getElementByClass('intro')
+const searchInput = getElementById('search')
+
+getElementById('search-form').addEventListener('submit', doSearch)
+
+const mediaLayout = data => {
+  const items = data.collection.items
+  console.log(items)
+  items.forEach(item => {
+    let media,
+      track,
+      h1 = createNode('h1'),
+      small = createNode('small'),
+      p = createNode('p'),
+      header = createNode('div', 'header'),
+      image = createNode('div', 'image'),
+      text = createNode('div', 'text'),
+      wrapper = createNode('div', 'wrapper')
+
+    item.data[0].media_type === 'image'
+      ? ((media = createNode('img')),
+        (media.src = item.links[0].href),
+        (media.alt = item.data[0].title))
+      : ((media = createNode('video')),
+        (track = createNode('track')),
+        ((track.src = `http://images-assets.nasa.gov/video/${
+          item.data[0].nasa_id
+        }/${item.data[0].nasa_id}.vtt`),
+        (media.src = `http://images-assets.nasa.gov/video/${
+          item.data[0].nasa_id
+        }/${item.data[0].nasa_id}~preview.mp4`)),
+        (media.controls = true),
+        getMediaType(item))
+
+    p.innerHTML = item.data[0].description
+    h1.innerHTML = item.data[0].title
+    small.innerHTML = '<br />' + item.data[0].date_created
+
+    item.data[0].media_type === 'video' && append(media, track)
+    append(h1, small)
+    append(header, h1)
+    append(image, media)
+    append(text, p)
+    append(wrapper, header)
+    append(wrapper, image)
+    append(wrapper, text)
+    append(intro, wrapper)
+  })
+}
+
+// TODO: SET VIDEO QUALITY
+// TODO: LOOK FOR .SRT TO .VTT
+// TODO: LOAD CAPTION IMAGE FIRST AND THEN VIDEO SRC
+// TODO: SEARCH MIX IMG AND VIDEO FUNC
